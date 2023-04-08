041f4c1100b346106cb3a3bfab14b856bdb1f3fe
chwah <ch.wahlen01@gmail.com>
2023-04-08 23:10:01 +0100

    Merge remote-tracking branch 'origin/main'

--- Focal/Focal_app/urls.py
+++ Focal/Focal_app/urls.py
@@ -5,5 +5,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="index"),
-    path('Activities/<int:activity_id>', views.activity_by_id, name='activity_by_id')
+    path('Activities/<int:activityId>', views.activity_by_id, name='activity_by_id')
 ]
--- Focal/Focal_app/views.py
+++ Focal/Focal_app/views.py
@@ -4,10 +4,10 @@ from django.http import HttpResponse
 from .models import Activities, Categories, UserActivities
 
 def index(request):
-    return HttpResponse("Hello world")
+    return render(request, 'Focal_app.html', {'username':'chwah'})
 
 
-def activity_by_id(request, id):
-    my_activity = Activities.objects.get(pk='activityId')
+def activity_by_id(request, activityId):
+    my_activity = Activities.objects.get(pk=activityId)
     return HttpResponse(f"Activity: {my_activity.name}")
 
