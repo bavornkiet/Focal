c391616d5d62f31690b02942b222416531411d01
kuter555 <75378784+kuter555@users.noreply.github.com>
2023-04-09 00:42:18 +0100

    Delete Focal directory

--- Focal/Focal_app/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class FocalAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'Focal_app'
--- Focal/Focal_app/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-from . import views
-
-urlpatterns = [
-    path('', views.index, name="index"),
-    path('Activities/<int:activityId>', views.activity_by_id, name='activity_by_id')
-]
